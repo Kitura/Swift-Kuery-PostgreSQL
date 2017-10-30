341832f0fd2444ba0dd7c0885d1acc3cee4292d9
ShihabMehboob <shihab.mehboob1@ibm.com>
2017-10-30 12:03:27 +0000

    Updated for Swift4

--- Package.swift
+++ Package.swift
@@ -25,13 +25,12 @@ let package = Package(
         .package(url: "https://github.com/IBM-Swift/CLibpq.git", .upToNextMinor(from: "0.1.0")),
         .package(url: "https://github.com/IBM-Swift/Swift-Kuery.git", .upToNextMinor(from: "0.13.0")),
         ],
-    exclude: ["Configuration", "Scripts"]
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftKueryPostgreSQL",
-            dependencies: ["SwiftKueryPostgreSQL"]
+            dependencies: ["SwiftKuery"]
         )
     ]
 )
