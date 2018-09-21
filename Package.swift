b65408ae388fac31bd8077b9bff59c8cb78b17dc
kilnerm <38245247+kilnerm@users.noreply.github.com>
2018-09-21 07:40:22 +0100

    Build column description in the plugin (#58)

--- Package.swift
+++ Package.swift
@@ -29,7 +29,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/IBM-Swift/CLibpq.git", .upToNextMinor(from: "0.1.0")),
-        .package(url: "https://github.com/IBM-Swift/Swift-Kuery.git", .branch("next")),
+        .package(url: "https://github.com/IBM-Swift/Swift-Kuery.git", .branch("issue_column_attributes") ),
+        //.package(url: "https://github.com/IBM-Swift/Swift-Kuery.git", .branch("next")),
         ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
