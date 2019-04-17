15f8b67cdf8df7b316c474e9c837f825c7c1718f
kilnerm@uk.ibm.com <kilnerm@uk.ibm.com>
2019-04-17 10:36:16 +0100

    temporarily update package.swift for travis

--- Package@swift-4.2.swift
+++ Package@swift-4.2.swift
@@ -28,7 +28,8 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/IBM-Swift/Swift-Kuery.git", from: "3.0.0"),
+        //.package(url: "https://github.com/IBM-Swift/Swift-Kuery.git", from: "3.0.0"),
+        .package(url: "https://github.com/IBM-Swift/Swift-Kuery.git", .branch("insert_nulls_property")),
         ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
