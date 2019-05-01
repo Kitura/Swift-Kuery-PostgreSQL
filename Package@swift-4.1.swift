e651582739d546b8eb71098cba2da9c962a5c4d6
kilnerm@uk.ibm.com <kilnerm@uk.ibm.com>
2019-05-01 09:19:11 +0100

    Update SwiftKuery dependancy to master

--- Package@swift-4.1.swift
+++ Package@swift-4.1.swift
@@ -29,8 +29,8 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/IBM-Swift/CLibpq.git", .upToNextMinor(from: "0.1.0")),
-        //.package(url: "https://github.com/IBM-Swift/Swift-Kuery.git", from: "3.0.0"),
-        .package(url: "https://github.com/IBM-Swift/Swift-Kuery.git", .branch("insert_null_prop")),
+        //.package(url: "https://github.com/IBM-Swift/Swift-Kuery.git", from: "3.1.0"),
+        .package(url: "https://github.com/IBM-Swift/Swift-Kuery.git", .branch("master")),
         ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
